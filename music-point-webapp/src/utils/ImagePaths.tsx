a7b671f8e43758eb70bb178be0649bcd5901ac4e
santosh <santhoshkrishna770@gmail.com>
2022-09-17 19:21:21 +0530

    image path constants

---
+++ music-point-webapp/src/utils/ImagePaths.tsx
@@ -0,0 +1,7 @@
+const iconsPath = "/media/icons/"
+
+export const linkIcon = iconsPath + "link.png"
+export const walletNotConnected = iconsPath + "wallet-not-connected.png"
+export const walletConnectionError = iconsPath + "wallet-connection-error.png"
+export const walletConnected = iconsPath + "wallet-connected.png"
+export const walletConnecting = iconsPath + "wallet-connecting.png"
