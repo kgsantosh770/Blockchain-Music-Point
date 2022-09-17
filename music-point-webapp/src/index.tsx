9f3f47c8947215148309a21a9aeaaef0e04a4698
santosh <santhoshkrishna770@gmail.com>
2022-09-17 15:24:28 +0530

    removed strictmode and implemented provider

--- music-point-webapp/src/index.tsx
+++ music-point-webapp/src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {WalletContextProvider} from "./WalletContext"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <WalletContextProvider>
     <App />
-  </React.StrictMode>
+  </WalletContextProvider>
 );
