b6b923ae66c4be6b5d31e2cac9527913c81ec745
santosh <santhoshkrishna770@gmail.com>
2022-10-05 10:33:35 +0530

    set time gap between music post

--- local-network-hardhat/contracts/MusicPoint.sol
+++ local-network-hardhat/contracts/MusicPoint.sol
@@ -5,8 +5,13 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract MusicPoint {
-    constructor() payable {}
-    
+    uint256 private seed;
+    mapping(address => uint) lastPostedAt;
+
+    constructor() payable {
+        seed = (block.difficulty + block.timestamp) % 100;
+    }
+
     event PostMusic(address indexed from, string musicUrl, uint256 postedTime);
 
     struct Music {
@@ -17,17 +22,34 @@ contract MusicPoint {
 
     Music[] musics;
 
+    function getRandomNumber() private view returns (uint256) {
+        return (block.timestamp + block.difficulty + seed) % 100;
+    }
+
+    function sendEther() public {
+        uint256 percentChance = 50;
+        uint256 randomNumber = getRandomNumber();
+        console.log("Random seed: ", randomNumber);
+        if (randomNumber < percentChance) {
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more amount than available amount"
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to send ether");
+        }
+    }
+
     function postMusic(string memory _url) public {
+        require(
+            lastPostedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait for 15 minutes between your posts."
+        );
+        lastPostedAt[msg.sender] = block.timestamp;
         musics.push(Music(msg.sender, _url, block.timestamp));
         emit PostMusic(msg.sender, _url, block.timestamp);
-
-        uint256 prizeAmount = 0.001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more amount than available amount"
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to send ether");
+        sendEther();
     }
 
     function getMusicCount() public view returns (uint256) {
--- local-network-hardhat/scripts/runMusicPoint.js
+++ local-network-hardhat/scripts/runMusicPoint.js
@@ -7,7 +7,7 @@ const main = async () => {
     await musicContract.deployed();
 
     console.log("Contract deployed to: ", musicContract.address);
-    console.log("Contract owner: ", owner)
+    console.log("Contract owner: ", owner.address)
 
     //Get Contract balance
     let contractBalance = await hre.ethers.provider.getBalance(musicContract.address);
@@ -41,19 +41,43 @@ const main = async () => {
         hre.ethers.utils.formatEther(contractBalance)
     );
 
-    //Post a music using another account
-    // console.log("Posting music as a random person ...");
-    // musicTxn = await musicContract.connect(randomPerson).postMusic("music url 1");
-    // await musicTxn.wait();
-    // console.log("Music posted successfully");
+    // Post a music using another account
+    console.log("Posting music as a random person ...");
+    musicTxn = await musicContract.connect(randomPerson).postMusic("music url 1");
+    await musicTxn.wait();
+    console.log("Music posted successfully");
 
-    // //Get total music count
-    // musicCount = await musicContract.getMusicCount();
-    // console.log("Total music: ", musicCount)
+    //Get total music count
+    musicCount = await musicContract.getMusicCount();
+    console.log("Total music: ", musicCount)
 
-    // //Get all musics
+    //Get all musics
     // allMusic = await musicContract.getAllMusic();
     // console.log("Musics posted till date: \n", allMusic);
+
+    //Get Contract balance
+    contractBalance = await hre.ethers.provider.getBalance(musicContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    //Post a music as owner before 15 min time gap
+    console.log("Posting music ...");
+    musicTxn = await musicContract.connect(owner).postMusic("music url 1");
+    await musicTxn.wait();
+    console.log("Music posted successfully");
+
+    //Get music count after posting
+    musicCount = await musicContract.getMusicCount();
+    console.log("Total music: ", musicCount)
+
+    //Get Contract balance
+    contractBalance = await hre.ethers.provider.getBalance(musicContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 }
 
 const runMain = async () => {
